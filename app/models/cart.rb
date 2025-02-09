fe6b4791055c7ad9937ff408bca6d3e0517201e4
Mauricio Gravena <mauricio.gravena@rebase.com.br>
2025-02-09 16:27:28 -0300

    feat: create new cart attributes

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,11 @@ class Cart < ApplicationRecord
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+  def mark_as_abandoned
+    update(abandoned: true) if last_interaction_at <= (DateTime.current - 3.hours)
+  end
+
+  def remove_if_abandoned
+    destroy if last_interaction_at <= (DateTime.current - 7.days)
+  end
 end
